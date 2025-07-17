ea21c05162161f07c89ac2946d7022c5766c0b99
Simon <mitterhofersimon@outlook.com>
2025-07-17 10:35:05 +0200

    Update and rename OICD_RUN.R to run.R

--- code/run.R
+++ code/run.R
@@ -3,8 +3,8 @@ library(shiny)
 library(shinydashboard)
 
 # Import UI and server ---------------------------------------------------------
-source('OICD_UI.R')
-source('OICD_server.R')
+source('user_interface.R')
+source('server.R')
 
 # Run application --------------------------------------------------------------
 shinyApp(ui = OICD_UI, server = OICD_server)
